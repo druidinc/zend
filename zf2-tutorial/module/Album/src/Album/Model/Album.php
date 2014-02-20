e395f03debae5e6f6c0e30e05740d9e5aed68126
Egee Boy Gutierrez <egeeboygutierrez91@gmail.com>
2014-02-20 23:48:49 +0800

    Added  more actions

--- zf2-tutorial/module/Album/src/Album/Model/Album.php
+++ zf2-tutorial/module/Album/src/Album/Model/Album.php
@@ -2,16 +2,92 @@
 
 namespace Album\Model;
 
-class Album
+use Zend\InputFilter\Factory as InputFactory;     // <-- Add this import
+use Zend\InputFilter\InputFilter;                 // <-- Add this import
+use Zend\InputFilter\InputFilterAwareInterface;   // <-- Add this import
+use Zend\InputFilter\InputFilterInterface;
+
+class Album implements InputFilterAwareInterface
 {
     public $id;
     public $artist;
     public $title;
+    protected $inputFilter;
 
     public function exchangeArray($data)
     {
         $this->id     = (!empty($data['id'])) ? $data['id'] : null;
         $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
         $this->title  = (!empty($data['title'])) ? $data['title'] : null;
+    }
+
+    // Add the following method:
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
+    }
+    
+    // Add content to this method:
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new \Exception("Not used");
+    }
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+            $factory     = new InputFactory();
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'id',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'Int'),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'artist',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+
+            $inputFilter->add($factory->createInput(array(
+                'name'     => 'title',
+                'required' => true,
+                'filters'  => array(
+                    array('name' => 'StripTags'),
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array(
+                            'encoding' => 'UTF-8',
+                            'min'      => 1,
+                            'max'      => 100,
+                        ),
+                    ),
+                ),
+            )));
+
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
     }
 }
